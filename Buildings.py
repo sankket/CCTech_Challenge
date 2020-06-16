35597e1f7c981a084601b2c05a31f22348a78544
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-06-16 16:01:26 +0530

    Update Buildings.py

--- Buildings.py
+++ Buildings.py
@@ -1,1 +1,27 @@
-
+# Python3 program to count buildings  
+# that can see sunlight. 
+  
+# Returns count buildings that 
+# can see sunlight 
+def countBuildings(arr, n): 
+  
+    # Initialuze result (Note that first  
+    # building always sees sunlight) 
+    count = 1
+  
+    # Start traversing element 
+    curr_max = arr[0] 
+    for i in range(1, n): 
+      
+        # If curr_element is maximum, 
+        # update maximum and increment count 
+        if (arr[i] > curr_max): 
+          
+            count += 1
+            curr_max = arr[i] 
+  
+    return count 
+   
+arr = [[[4,0],[4,-5],[7,-5],[7,0]], [[0.4,-2],[0.4,-5],[2.5,-5],[2.5,-2]]] 
+n = len(arr) 
+print(countBuildings(arr, n)) 
